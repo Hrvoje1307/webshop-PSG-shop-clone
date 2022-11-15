468d956d8685ad7de8eb916b9680a98929385aa9
Hrvooje <cuckovichrvoje@gmail.com>
2022-11-15 16:59:08 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Psg fan shop</title>
+        <link rel="stylesheet" href="./css/header.css">
+        <link rel="icon" href="./images/psg.png">
+
+        <!-- BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <header><?php require_once("./php/header.php");?></header>
+    
+    
+        <!-- JavaScript-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <script src="./js/script.js"></script>
+    </body>
+</html>
