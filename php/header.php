c42fae7d4685c0a5fca87fe188f442c26490d8eb
Hrvooje <cuckovichrvoje@gmail.com>
2022-11-17 15:31:00 +0100

    dorađen menu i dodan naslov

--- php/header.php
+++ php/header.php
@@ -1,12 +1,34 @@
-<div class="container-fluid">
+<div class="container-fluid text-center">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="logo-section">
-                <img src="./images/logo.png" alt="logo">
+            <a href="#Home"><img src="./images/logo.png" alt="logo"></a>
             </div>
         </div>
-        <div class="col-md-4"></div>
-        <div class="col-md-4"></div>
+        <div class="col-md-6">
+            <div class="menu">
+                <ul>
+                    <li><a href="#shop">Shop</a></li>
+                    <li><a href="#players">Players</a></li>
+                    <li><a href="#contact">Contact us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="btns">
+                <button class="log-in"><img src="./images/account.png" alt="log-in"></button>
+                <button class="cart"><img src="./images/shopping-cart.png" alt="cart"></button>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="heading">
+                <h1>Ici c'est Paris</h1>
+            </div>
+        </div>
+        <div class="col-md-2"></div>
     </div>
 </div>
 
